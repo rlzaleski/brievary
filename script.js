3f4badf326e5b8cca03b221b8834536cbf59443f
Robert Zaleski <rzaleski@franciscan.edu>
2024-02-24 19:59:58 -0500

    Start creating site.

---
+++ script.js
@@ -0,0 +1,15 @@
+
+function getContent() {
+  var http_request;
+  http_request = new XMLHttpRequest();
+  http_request.onreadystatechange = function () { 
+    console.log('request: ',http_request.readyState); 
+    if (http_request.readyState == 4) {
+      console.log(http_request.response);
+    }
+  };
+  http_request.open("GET", "http://www.ibreviary.com/m2/breviario.php?s=vespri");
+  http_request.withCredentials = false;
+  http_request.setRequestHeader("Content-Type", "text/html");
+  http_request.send();
+}
